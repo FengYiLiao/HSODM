1ea432c11c18d3c16788ab3e13af686d01867ecc
FengYiLiao <robert147456@gmail.com>
2023-08-13 09:59:01 -0700

    Update Test_HSODM.m

--- Test_HSODM.m
+++ Test_HSODM.m
@@ -34,12 +34,12 @@ function y = routine(x,Xk,prob,para) %Eigenvector routine  %Xk is current iterat
     gk       = prob.M.egrad2rgrad(Xk,prob.egrad(Xk)); 
     x1       = reshape(x(1:end-1),prob.n,prob.rank);
     x2       = x(end);   
-    y        = zeros(prob.n*prob.rank+1,1);
-    temp     = prob.M.ehess2rhess(Xk,prob.egrad(Xk),prob.ehess(Xk,x1),x1)+para.delta*gk;
-    y(1:end-1) = temp(:);
-    y(end)   = gk(:).'*x1(:) - para.delta*x2;
-%     y = [reshape(prob.M.ehess2rhess(Xk,prob.egrad(Xk),prob.ehess(Xk,x1),x1)+para.delta*gk,[],1);
-%         gk(:).'*x1(:) - para.delta*x2];
+%     y        = zeros(prob.n*prob.rank+1,1);
+%     temp     = prob.M.ehess2rhess(Xk,prob.egrad(Xk),prob.ehess(Xk,x1),x1)+para.delta*gk;
+%     y(1:end-1) = temp(:);
+%     y(end)   = gk(:).'*x1(:) - para.delta*x2;
+    y = [reshape(prob.M.ehess2rhess(Xk,prob.egrad(Xk),prob.ehess(Xk,x1),x1)+para.delta*gk,[],1);
+        gk(:).'*x1(:) - para.delta*x2];
 end
 
 function y = cost(X,C)
