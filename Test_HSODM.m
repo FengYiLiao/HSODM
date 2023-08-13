68b8eb007bfce871033bd8b32be4a713e0432414
FengYiLiao <robert147456@gmail.com>
2023-08-13 10:01:35 -0700

    Update Test_HSODM.m

--- Test_HSODM.m
+++ Test_HSODM.m
@@ -8,29 +8,29 @@ dataroot = "data\sdplib\";
 load(dataroot+set{1});
 %load(dataroot+"G1.mat");
 
-para.epislon   = 10^-6;%desired gradient accuracy
-para.Maxiter   = 1000;%Maximum iterations
-para.beta      = 0.5;%line search parameter: reduction
-para.gamma     = 2;%line search parameter: a constant
-para.Threshold = 2; %This is cap delta (trigangle) in the paper
+para.epislon   = 10^-6; %desired gradient accuracy
+para.Maxiter   = 1000;  %Maximum iterations
+para.beta      = 0.5;   %line search parameter: reduction
+para.gamma     = 2;     %line search parameter: a constant
+para.Threshold = 2;     %This is cap delta (trigangle) in the paper
 para.nu        = 0.45;
-para.delta     = 2; %the button right constant (control eigenvalue)
-para.eta       = 1; %initial line search step size
+para.delta     = 2;     %the button right constant (control eigenvalue)
+para.eta       = 1;     %initial line search step size
 
-prob.n       = height(C);
-prob.rank    = 15;
-prob.M       = obliquefactory(prob.rank,prob.n,true); %Create a mainfold
-prob.cost    = @(X) cost(X,C);
-prob.egrad   = @(X) 2*C*X; %euclidean gradient
-prob.ehess   = @(X, U) 2*U;%euclidean hessian
-prob.routine = @routine; %power method routine
+prob.n         = height(C);
+prob.rank      = 15;
+prob.M         = obliquefactory(prob.rank,prob.n,true); %Create a mainfold
+prob.cost      = @(X) cost(X,C);
+prob.egrad     = @(X) 2*C*X;                            %euclidean gradient
+prob.ehess     = @(X, U) 2*U;                           %euclidean hessian
+prob.routine   = @routine;                              %power method routine
 
 Out = HSODM(prob,para);  %main function 
 
 
 
 
-function y = routine(x,Xk,prob,para) %Eigenvector routine  %Xk is current iterate
+function y = routine(x,Xk,prob,para) %power method routine  %Xk is current iterate
     gk       = prob.M.egrad2rgrad(Xk,prob.egrad(Xk)); 
     x1       = reshape(x(1:end-1),prob.n,prob.rank);
     x2       = x(end);   
