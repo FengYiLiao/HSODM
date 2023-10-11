806a7395c3802136fe6381a5dbacb9f84d0a6e84
FengYiLiao <robert147456@gmail.com>
2023-10-10 18:15:56 -0700

    update

--- result/Copy_of_ShowResult_MaxCut.m
+++ result/Copy_of_ShowResult_MaxCut.m
@@ -4,11 +4,21 @@ set= {'mcp100.mat','mcp124-1.mat','mcp124-2.mat','mcp124-3.mat','mcp124-4.mat','
   
   %fprintf("  Name   |    RTR    |  HSODM-1   |  HSODM-10  |   HSODM-D2 \n")
   fprintf("  Name   |    RTR    |  HSODM  \n")
-  for i = 1:length(set)
+  for i = 1%:length(set)
     name  = split(set{i},'.');
+    load("D:\UCSD\Research\HSODM\data\sdplib\"+name{1}+".mat");
     T     = load("manopt\MaxCut\"+name{1}+"-result.mat");
     Out1  = load("hsodm\MaxCut\eta1\"+name{1}+"-result.mat");
     fprintf("%6s      %3d       %3d\n",name{1},T.info(end).iter,Out1.Out.iter);
+    semilogy(1:length(Out1.Out.obj),Out1.Out.obj-Truecost);
+    
+    CostRT = zeros(length(T.info),1);
+    for j = 1 : length(T.info)
+        CostRT(j) = T.info(j).cost;
+    end
+    hold on;
+    semilogy(1:length(CostRT),abs(CostRT-Truecost));
+    %plot(Out1.Out.obj-Truecost);
 %     Out10 = load("hsodm\eta10\"+name{1}+"-result.mat");
 %     OutD2 = load("hsodm\d2\"+name{1}+"-result.mat");
 %     fprintf("%6s      %6d       %6d        %6d     %6d\n",name{1},T.info.iter,Out1.Out.iter,Out10.Out.iter,OutD2.Out.iter);
